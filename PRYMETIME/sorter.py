1c9ab008cf732db4c1c2a3d2d5974f1d9868533b
jcollins1x <jcollins1x@gmail.com>
2020-02-07 07:23:03 -0500

    Update sorter.py

--- PRYMETIME/sorter.py
+++ PRYMETIME/sorter.py
@@ -19,19 +19,28 @@ circ_D = {}
 for x in range(0, len(df.index)):
     circ_D[df.loc[x,"#seq_name"]] = df.loc[x,"circ."]
 
-### Make a fasta file of only the circular contigs ###
-######################################################
-fasta_sequences = SeqIO.parse(open("scaffolds.fasta"),'fasta')
+### Make a dictionary of repetitive or linear from Flye output info file ###
+##########################################################################
+df = pd.read_csv("assembly_info.txt",sep='\t')
 
-cir_seqs = [x for x in fasta_sequences if circ_D[x.id] is "+"]
+rep_D = {}
+
+for x in range(0, len(df.index)):
+    rep_D[df.loc[x,"#seq_name"]] = df.loc[x,"repeat"]
+
+### Make a fasta file of only the circular and repetitive contigs ###
+######################################################
+fasta_sequences = SeqIO.parse(open("assembly.fasta"),'fasta')
+
+cir_seqs = [x for x in fasta_sequences if circ_D[x.id] is "+" or rep_D[x.id] is "+"]
 
 SeqIO.write(cir_seqs , "cir_contigs.fasta", "fasta")
 
 ### Make a fasta file of only the linear contigs ###
 ####################################################
-fasta_sequences = SeqIO.parse(open("scaffolds.fasta"),'fasta')
+fasta_sequences = SeqIO.parse(open("assembly.fasta"),'fasta')
 
-lin_seqs = [x for x in fasta_sequences if circ_D[x.id] is '-']
+lin_seqs = [x for x in fasta_sequences if circ_D[x.id] is '-' and rep_D[x.id] is '-']
 
 SeqIO.write(lin_seqs , "lin_contigs.fasta", "fasta")
 
