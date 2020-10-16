df51386d951b110f2cb3f0fe2c3f03b0418d1a6d
emyounglab <61199405+emyounglab@users.noreply.github.com>
2020-10-16 10:51:25 -0400

    Create eng_sig_cmap.sh

---
+++ PRYMETIME/eng_sig_cmap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J eng_sig_figure
+
+EXECDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+PREFIX=$(basename "$1"
+
+cp "$EXECDIR"/eng_sig_cmap.R "$1"
+cd "$1"
+
+cp "$PREFIX"_final.bed genome.bed
+cp "$PREFIX"_cmap.txt cmap.txt
+Rscript eng_sig_cmap.R "$1"
