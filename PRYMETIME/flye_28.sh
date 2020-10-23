e84a5f9f7f3b3cbb98a487b1821648fd7d8a0841
emyounglab <61199405+emyounglab@users.noreply.github.com>
2020-10-23 11:56:13 -0400

    Update flye_28.sh

--- PRYMETIME/flye_28.sh
+++ PRYMETIME/flye_28.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 16
-#SBATCH --mem-per-cpu=20000MB
-#SBATCH -o flye_%j.out
-#SBATCH -e flye_%j.err
-#SBATCH -J flye
 
-flye --nano-raw "$1" --genome-size "$2" --meta --plasmids -o "$3" --threads 128
+flye --nano-raw "$1" --genome-size "$2" --meta --plasmids -o "$3"
