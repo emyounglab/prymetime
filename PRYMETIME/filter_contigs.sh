c1174b726ecec938f1e84f67dc15af38dcf9c28f
Tyler Marshall <tyler.e.marshall@raytheon.com>
2022-01-31 15:26:47 -0500

    Rename bowtie2 index
    Giving it a more context descriptive name because another index
    will be added soon.

--- PRYMETIME/filter_contigs.sh
+++ PRYMETIME/filter_contigs.sh
@@ -15,11 +15,11 @@ EXECDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 cd "$4"
 
 log "Building index"
-bowtie2-build --threads ${N_THREADS} "$1" refgenome
+bowtie2-build --threads ${N_THREADS} "$1" flye-idx
 echo "Done"
 
 log "Mapping reads"
-bowtie2 -x refgenome --threads ${N_THREADS} --no-unal -1 "$2" -2 "$3" -S - \
+bowtie2 -x flye-idx --threads ${N_THREADS} --no-unal -1 "$2" -2 "$3" -S - \
     | samtools view --threads ${N_THREADS} -b - \
     | samtools sort --threads ${N_THREADS} -m 5G - -o mapping_result_sorted.bam
 log "Done"
@@ -31,4 +31,4 @@ log "Filtering contigs"
 samtools mpileup mapping_result_sorted.bam | python3 "${EXECDIR}/filter_contigs.py" accept.fasta reject.fasta ${MIN_COVERAGE_DEPTH}
 log "Done"
 
-rm -f refgenome*.bt2
+rm -f flye-idx*.bt2
